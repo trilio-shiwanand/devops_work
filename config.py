9a93491b522b94d29f1b435b38105b3ebf57e471
trilio-shiwanand <shiwanand.bicchewar@trilio.io>
2021-08-23 18:06:30 +0530

    Geta last successful jobs data

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ jenkins_password="ecf7f31d87b44afeac3c51f4c5a3f783"
 jenkins_dirname="SHIWA-SCRIPTED-DSL"
 jenkins_jobname = "Canonical_ussuri_sanity"
 jenkins_jobsubstr="_sanity"
-
-
 jenkins_url_dummy = 'http://192.168.16.188:8080/job/SHIWA-SCRIPTED-DSL/job/Canonical_ussuri_sanity/api/json'
+jenkins_outputfile = "./jenkins_lastsuccessful_jobs_data.txt"
 
--- jenkins_jobs_details.py
+++ jenkins_jobs_details.py
@@ -1,39 +1,141 @@
-#!/usr/bin/env python3
+#!/bin/python
 # -*- coding: utf-8 -*-
 
-
-import sys
 import requests
+import json
 import config
-import jenkinsapi
+import tabulate
+import codecs
+from datetime import datetime
 
-
-
-JENKINS_SERVER = config.jenkins_server
+## configuration data from config.py file
+JENKINS_URL = config.jenkins_url
 JENKINS_USERNAME = config.jenkins_username
 JENKINS_PASSWORD = config.jenkins_password
 JENKINS_DIRNAME = config.jenkins_dirname
 JENKINS_JOBNAME = config.jenkins_jobname
+JENKINS_JOBS_SUBSTR = config.jenkins_jobsubstr
+JENKINS_OUTPUTFILE = config.jenkins_outputfile
 
-print(JENKINS_SERVER)
-#print ('\n'JENKINS_SERVER'\nJENKINS_USERNAME\nJENKINS_PASSWORD\nJENKINS_DIRNAME\nJENKINS_JOBNAME')
+from jenkinsapi.jenkins import Jenkins
 
-
-# variable used for max no. of abort jobs to pick committed changes
 def get_server_instance():
-    jenkins_url = JENKINS_SERVER
-    server = jenkins.Jenkins(jenkins_url, username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
+    jenkins_url = JENKINS_URL
+    server = Jenkins(jenkins_url, username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
     return server
+print (get_server_instance())
 
+def get_job_details():
+    server = get_server_instance()
+    for job_name, job_instance in server.get_jobs():
+        all_job_name_list=job_instance.name
+        # print('all job: ' + str(all_job_name_list))
+        if not (not (JENKINS_JOBS_SUBSTR is not None) or not (JENKINS_JOBS_SUBSTR in all_job_name_list)):
+            # print('Job Name: '+str(all_job_name_list))
+            base_url = ('%(u)s/job/%(d)s/job/%(j)s' % {'u': JENKINS_URL, 'd': JENKINS_DIRNAME, 'j': all_job_name_list})
+            username = JENKINS_USERNAME
+            password = JENKINS_PASSWORD
+            url = (base_url + '/api/json')
+            #print('\nBase_url :' + str(base_url))
+            response = requests.get(url, auth=(username, password), verify=False)
+            ##print(str(response))
+            f = open(JENKINS_OUTPUTFILE, 'a+')
+            try:
+                buildnumberJson = json.loads(response.text)
+            except:
+                print("Failed to parse json")
 
+            if "lastBuild" in buildnumberJson:
+                totalbuilds = buildnumberJson["lastBuild"]
+                runs = totalbuilds["number"]
+            else:
+                print("Failed to get build")
 
-server = jenkins.Jenkins(JENKINS_SERVER, username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
-current_build_number = server.get_job_info(JENKINS_JOBNAME)['lastBuild']['number']
-current_build_info = server.get_build_info(JENKINS_JOBNAME, current_build_number)
+            # Iterate over the job build runs to get the build status for each
+            lastSuccessfulBuildurl = (base_url + '/lastSuccessfulBuild/api/json')
+            last_success_job_data = []
+            print('Last Successful Build Json: ' + str(lastSuccessfulBuildurl))
+            print('Job Name: ' + str(all_job_name_list))
+            f.write('\nLast Successful Build Json: ' + str(lastSuccessfulBuildurl))
+            f.write('\nJob Name: ' + str(all_job_name_list))
 
-print(current_build_number)
-print(current_build_info)
+            try:
+                response = requests.get(lastSuccessfulBuildurl, auth=(username, password), verify=False)
+                last_success_job_data = json.loads(response.text)
+                result = last_success_job_data['result']
+                #print('Last Build Result: ' + str(result))
 
+            except Exception as e:
+                print(str(e))
+            if "result" in last_success_job_data:
+                if last_success_job_data["result"] == "SUCCESS":
+                    result = last_success_job_data['result']
+                    build_no = last_success_job_data['id']
+                    executor = last_success_job_data['executor']
+                    full_disp_name = last_success_job_data['fullDisplayName']
+                    date = last_success_job_data['timestamp']
 
-if __name__ == '__main__':
-    print (get_server_instance().version)
+                    # date = datetime.datetime.fromtimestamp(date/1000)
+                    #get_all_param = last_success_job_data['actions'][0]['parameters']
+                    #get_param = [dict(class_name=k1["_class"], name=k1["name"], value=k1["value"]) for k1 in get_all_param]
+                    #key_value = [dict(name=k1["name"], value=k1["value"]) for k1 in get_param]
+
+                    print('Job FullName: ' + str(full_disp_name))
+                    print('Job Result: ' + str(result))
+                    print('Executor Name: ' + str(executor))
+                    print('Last Successful Build No: ' + str(build_no))
+                    print('Date: ' + str(date))
+                    # f = open(JENKINS_OUTPUTFILE, 'a+')
+                    f.write('\nJob FullName: ' + str(full_disp_name)+'\n')
+                    f.write('Job Result: ' + str(result)+'\n')
+                    f.write('Executor Name: ' + str(executor)+'\n')
+                    f.write('Last Successful Build No: ' + str(build_no)+'\n')
+                    f.write('Date: ' + str(date)+'\n')
+                    # if get_all_param != 'NULL':
+                    #     print('\nParameters are as belows')
+                    #     header = key_value[0].keys()
+                    #     rows = [x.values() for x in key_value]
+                    #     print(tabulate.tabulate(rows, header))
+                    #     print('--------------------****----------------------\n')
+                    # elif get_all_param == 'NULL':
+                    #     print('Parameters not found')
+                    #     print('--------------------****----------------------')
+                    if (int(build_no) <= 1):
+                        get_all_param = last_success_job_data['actions'][0]
+                        get_param = [dict(class_name=k1["_class"], name=k1["name"], value=k1["value"]) for k1 in get_all_param]
+                        key_value = [dict(name=k1["name"], value=k1["value"]) for k1 in get_param]
+                        print('\nParameters are as belows')
+                        f.write('Parameters are as belows\n')
+                        header = key_value[0].keys()
+                        rows = [x.values() for x in key_value]
+                        print(tabulate.tabulate(rows, header))
+                        f.write('\n' + str(tabulate.tabulate(rows, header)))
+                        print('--------------------****----------------------\n')
+                        f.write('\n-----------------****--------------------')
+                        f = f.close(JENKINS_OUTPUTFILE)
+                    elif(int(build_no) > 2):
+                        get_all_param = last_success_job_data['actions'][0]['parameters']
+                        get_param = [dict(class_name=k1["_class"], name=k1["name"], value=k1["value"]) for k1 in get_all_param]
+                        key_value = [dict(name=k1["name"], value=k1["value"]) for k1 in get_param]
+                        key_value = [dict(name=k1["name"], value=k1["value"]) for k1 in get_param]
+                        print('\nParameters are as belows')
+                        f.write('\nParameters are as belows')
+                        header = key_value[0].keys()
+                        rows = [x.values() for x in key_value]
+                        print(tabulate.tabulate(rows, header))
+                        f.write('\n'+str(tabulate.tabulate(rows, header)))
+                        print('--------------------****----------------------\n')
+                        f.write('\n------------------------------------------')
+
+                    else:
+                        print('Job is not executed so far')
+                        f.write('\nJob is not executed so far')
+                        f = f.close(JENKINS_OUTPUTFILE)
+                if last_success_job_data["result"] == "FAILURE":
+                    print('Last Build Result Is Failed')
+                    f.write('\nLast Build Result Is Failed')
+                    f = f.close(JENKINS_OUTPUTFILE)
+
+print (get_job_details())
+## Write Data into the file
+print ('Done...')
